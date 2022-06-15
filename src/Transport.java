883f7b618798635dab74146f4319455f38e9e5f5
Lugas90 <Lugas90@mail.ru>
2022-06-15 21:27:00 +0300

    ДЗ урока 2.3

--- src/Transport.java
+++ src/Transport.java
@@ -1,14 +1,34 @@
-public class Transport {
+public class Transport implements ServiceStationCheck {
 
-    protected String modelName;
+    private String modelName;
 
-    protected int wheelsCount;
+    private int wheelsCount;
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public int getWheelsCount() {
+        return wheelsCount;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public void setWheelsCount(int wheelsCount) {
+        this.wheelsCount = wheelsCount;
+    }
 
     public void updateTyre() {
         System.out.println("Меняем покрышку");
     }
     public void checkEngine() {
         System.out.println("Проверяем двигатель");
+    }
+    @Override
+    public void check(Car car, Bicycle bicycle, Truck truck) {
+        ServiceStationCheck.super.check(car, bicycle, truck);
     }
 }
 
